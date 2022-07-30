7c32db09abe08af8ce2ec5d438ad51b82f838bbd
Jiho Han <jiho.han@cantab.net>
2022-07-30 23:31:34 +0200

    Set up repo and make package

---
+++ transition_dynamic_simulator/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="transition_dynamic_simulator",
+    version="0.1",
+    description="Simulation for transition dynamics for lasers, chemical reactions, etc.",
+    author="Jiho Han",
+    author_email="jiho.han@cantab.net",
+    license="MIT",
+    packages=["transition_dynamic_simulator"],
+)
